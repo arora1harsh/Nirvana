f60e6358d1513027f061b9a652bfab24295c14c6
arora1harsh <91772638+arora1harsh@users.noreply.github.com>
2025-04-06 14:28:04 +0530

    Update NirvanaFest.jsx
    Countdown

--- nirvana-fest/src/NirvanaFest.jsx
+++ nirvana-fest/src/NirvanaFest.jsx
@@ -10,7 +10,7 @@ export default function NirvanaFest() {
 
   // Countdown Logic
   useEffect(() => {
-    const targetDate = new Date("2025-04-10T00:00:00");
+    const targetDate = new Date("2025-04-17T00:00:00");
 
     const interval = setInterval(() => {
       const now = new Date();
